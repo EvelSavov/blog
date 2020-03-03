4ea646f7b70caf902dcc4f891d93cdd6d770cb5f
EvelSavov <evel_savov@abv.bg>
2020-03-03 18:25:05 +0200

    export post as txt file

--- target/classes/static/js/addFiles.js
+++ target/classes/static/js/addFiles.js
@@ -26,6 +26,13 @@ function readFileContent(file) {
 }
 
 
+
+
+
+
+
+
+
 function previewImages() {
 
   var $preview = $('#preview').empty();
@@ -50,3 +57,53 @@ function previewImages() {
 }
 
 document.querySelector('#files').addEventListener("change", previewImages);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function saveTextAsFile()
+{
+
+
+	var Title = document.getElementById("title").value;
+	var Body = document.getElementById("body").value;
+	var textToWrite = Title + "\n\n" + Body;
+	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+	var fileNameToSaveAs = "post";
+
+	var downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	if (window.webkitURL != null)
+	{
+		// Chrome allows the link to be clicked
+		// without actually adding it to the DOM.
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+	}
+	else
+	{
+		// Firefox requires the link to be added to the DOM
+		// before it can be clicked.
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = destroyClickedElement;
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+	}
+
+	downloadLink.click();
+}
+
+
+
